feb2f6d906cfe01af74241404c7700c46a56cff1
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-10 19:41:32 +0100

    --histogram argument

--- atmung/mne_atmung.py
+++ atmung/mne_atmung.py
@@ -30,12 +30,13 @@ def plot_resp(times, peak_times, resp):
     plt.plot(times, resp)
 
 
-def process(fname, plot=False):
+def process(fname, plot=False, histogram=False):
     print("Loading", fname)
     data, times, raw = load_vhdr(fname)
     datavector = data.reshape(-1)
     threshold = (max(datavector)-min(datavector)) / 30
     print("threshold", threshold)
+    print("standard deviation", datavector.std())
 
     # Einspeisen des Datenvektors in peakfinder 
     peak_loc, peak_mag = peak_finder(datavector,thresh=threshold, extrema=-1)
@@ -49,6 +50,10 @@ def process(fname, plot=False):
         plt.figure(path.basename(fname)+ ": resp")
         plot_resp(times, peak_times, datavector)
 
+    if histogram:
+        plt.figure(path.basename(fname)+ ": resp distribution")
+        plt.hist(datavector, bins=np.linspace(-1.5, 1, num=100), density=True)
+
 
 if __name__ == '__main__':
     import argparse
@@ -60,9 +65,11 @@ if __name__ == '__main__':
                    default=path.join(data_dir, 'BekJan', 'HOAF_EDA_Resp0002.vhdr'))
     p.add_argument('-p', '--plot', action='store_true',
                    help='Plotte die Resp-Kurve')
+    p.add_argument('-H', '--histogram', action='store_true',
+                   help='Plotte die Verteilung der Resp-Kurve')
     args = p.parse_args()
 
-    process(args.file, plot=args.plot)
+    process(args.file, plot=args.plot, histogram=args.histogram)
     # plot_test_range()
     plt.show()
 
