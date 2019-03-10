ae708ed1baba962a6a748a276385f352664f19ba
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-10 19:09:45 +0100

    started main method process(..)

--- atmung/mne_atmung.py
+++ atmung/mne_atmung.py
@@ -6,24 +6,18 @@ from resp import load_vhdr
 import numpy as np
 
 
-def plot_test_range(a=3000, b=3300):
+def plot_test_range(times, a=3000, b=3300):
     """
     Plot the respiration data in the range from a to b (seconds)
     """
     a = np.where(times >= a)[0][0]
     b = np.where(times <= b)[0][-1]
-    a = 0; b = len(times)
     tresp = resp[a:b]
     thres = default
     tpeak_loc, _ = peak_finder(tresp, thresh=thres, extrema=-1)
     tpeak_times = times[a:b][tpeak_loc]
-    if False:
-        plt.vlines(x=tpeak_times, ymin=-1, ymax=1)
-        plt.plot(times[a:b], tresp)
-
-    plt.xlabel(u"Intervalllänge")
-    plt.hist(np.diff(tpeak_times), bins=np.linspace(0, 7.5, num=100), 
-             label="Verteilung der Atmungsintervalllaengen", density=True)
+    plt.vlines(x=tpeak_times, ymin=-1, ymax=1)
+    plt.plot(times[a:b], tresp)
 
 
 def plot_resp(times, peak_times, resp):
@@ -32,25 +26,30 @@ def plot_resp(times, peak_times, resp):
     plt.plot(times, resp)
 
 
+def process(fname):
+    print("Loading", fname)
+    data, times, raw = load_vhdr(fname)
+    datavector = data.reshape(-1)
+    threshold = (max(datavector)-min(datavector))/30
+
+    # Einspeisen des Datenvektors in peakfinder 
+    peak_loc, peak_mag = peak_finder(datavector,thresh=threshold, extrema=-1)
+    peak_times = times[peak_loc]
+    # resp = raw.get_data()[0]
+    plt.xlabel(u"Intervalllänge")
+    plt.hist(np.diff(peak_times), bins=np.linspace(0, 7.5, num=100), 
+             label="Verteilung der Atmungsintervalllaengen", density=True)
+
+
 if __name__ == '__main__':
     import sys
 
     fname = '/home/jan/Documents/Doktorarbeit/Daten/BekJan/HOAF_EDA_Resp0002.vhdr'
     if len(sys.argv) >= 2:
         fname = sys.argv[1]
-        
-    print("Loading", fname)
-    data, times, raw = load_vhdr(fname)
-    # TODO: 20218200 = ??
-    datavector = data.reshape(-1)
-    default = (max(datavector)-min(datavector))/30
 
-    # Einspeisen des Datenvektors in peakfinder 
-    peak_loc, peak_mag = peak_finder(datavector,thresh=default, extrema=-1)
-    peak_times = times[peak_loc]
-    resp = raw.get_data()[0]
-
-    plot_test_range()
+    process(fname)
+    # plot_test_range()
     plt.show()
 
 
