9f8fbc6350fbd4cd6882262e8718bd97a82d4844
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-10 22:30:16 +0100

    block detection seems to work

--- atmung/blocks.py
+++ atmung/blocks.py
@@ -15,19 +15,37 @@ def plot_block_hist(ann):
     plt.hist(response_times, cumulative=True, bins=200)
 
 
-def find_blocks(ann):
+def find_blocks(ann, delta=1.5):
+    """
+    TODO
+    """
     mask = ann.description == RESPONSE
-    s0 = np.argmax(ann.description == r)
-    s3 = len(ann) - np.argmax(np.flipud(ann.description == RESPONSE)) - 1
+    s0 = np.argmax(mask)
+    t2 = len(ann) - np.argmax(np.flipud(mask)) - 1
 
     response_times = ann.onset[mask]
-    np.where(np.diff(response_times) > 1.5)[0]
-    s1, s2 = np.where(np.diff(response_times) > 1.5)[0]
+    a, b = np.where(np.diff(response_times) > delta)[0]
+    t0 = np.where(mask)[0][a]
+    s1 = np.where(mask)[0][a + 1]
+    t1 = np.where(mask)[0][b]
+    s2 = np.where(mask)[0][b + 1]
+    return [[s0, t0], [s1, t1], [s2, t2]]
+
 
 if __name__ == '__main__':
     fname = "../../Daten/BekJan/HOAF_05.vhdr"
     data, times, raw = load_vhdr(fname)
     ann = raw.annotations
-    plot_block_hist(ann)
+
+    blocks = np.array(find_blocks(ann))
+    print(blocks)
+    print(blocks.ravel())
+    print(ann.onset[blocks.ravel()])
+
+    raw.plot()
     plt.show()
+    
+    if False:
+        plot_block_hist(ann)
+        plt.show()
     
