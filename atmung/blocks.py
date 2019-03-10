5003f93c9a099a43f1ed376ba2cc5e6404e74cd7
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-10 22:11:28 +0100

    wip

--- atmung/blocks.py
+++ atmung/blocks.py
@@ -3,17 +3,31 @@ Find the Response blocks
 """
 from resp import load_vhdr
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_block_hist(raw, r='Response/R128'):
-    ann = raw.annotations
-    response_times = ann.onset[ann.description == r]
+RESPONSE = 'Response/R128'
+
+
+def plot_block_hist(ann):
+    mask = ann.description == RESPONSE
+    response_times = ann.onset[mask]
     plt.hist(response_times, cumulative=True, bins=200)
 
+
+def find_blocks(ann):
+    mask = ann.description == RESPONSE
+    s0 = np.argmax(ann.description == r)
+    s3 = len(ann) - np.argmax(np.flipud(ann.description == RESPONSE)) - 1
+
+    response_times = ann.onset[mask]
+    np.where(np.diff(response_times) > 1.5)[0]
+    s1, s2 = np.where(np.diff(response_times) > 1.5)[0]
 
 if __name__ == '__main__':
     fname = "../../Daten/BekJan/HOAF_05.vhdr"
     data, times, raw = load_vhdr(fname)
-    plot_block_hist(raw)
+    ann = raw.annotations
+    plot_block_hist(ann)
     plt.show()
     
