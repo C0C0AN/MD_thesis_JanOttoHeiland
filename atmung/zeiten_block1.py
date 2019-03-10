3e3e87e0214841b2827f2f05601b90a78fcb05f4
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-10 18:17:46 +0100

    cleaned up mne_atmung.py

---
+++ atmung/zeiten_block1.py
@@ -0,0 +1,125 @@
+"""
+Zeiten der Blöcke im ersten Stressblock.
+"""
+    # #time rc1 start
+    # rc1a = times[5365177]+6
+
+
+    # # In[50]:
+
+
+    # #time rc1 stop
+    # 1rc1e = times[5365177]+66
+
+
+    # # In[51]:
+
+
+    # #time mc1 start
+    # 1mc1a = times[5365177]+94
+
+
+    # # In[52]:
+
+
+    # #time mc1 stop
+    # 1mc1e = times[5365177]+152
+
+
+    # # In[53]:
+
+
+    # #time rs1 start
+    # 1rs1a = times[5365177]+182
+
+
+    # # In[54]:
+
+
+    # #time rs1 stop
+    # 1rs1e = times[5365177]+242
+
+
+    # # In[55]:
+
+
+    # #time ms1 start
+    # 1ms1a = times[5365177]+270
+
+
+    # # In[57]:
+
+
+    # #time ms1 stop
+    # 1ms1e = times[5365177]+330
+
+
+    # # In[ ]:
+
+
+    # #time rc2 start
+    # 1rc2a = times[5365177]+358
+
+
+    # # In[58]:
+
+
+    # #time rc2 stop
+    # 1rc2e = times[5365177]+413
+
+
+    # # In[59]:
+
+
+    # #time mc2 start
+    # 1mc2a = times[5365177]+446
+
+
+    # # In[60]:
+
+
+    # #time mc2 stop
+    # 1mc2e = times[5365177]+501
+
+
+    # In[61]:
+
+
+    #time rs2 start
+    # 1rs2a = times[5365177]+534
+
+
+    # In[62]:
+
+
+    # #time rs2 stop
+    # 1rs2e = times[5365177]+594
+
+
+    # # In[63]:
+
+
+    # #time ms2 start
+    # 1ms2a = times[5365177]+622
+
+
+    # # In[67]:
+
+
+    # #time ms2 stop
+    # 1ms2e = times[5365177]+682
+
+
+    # # In[65]:
+
+
+    # #Zeitspanne erster Stressblock
+    # times[8982913]-times[5365177]
+
+
+    # # In[40]:
+
+
+    # #Zeitspanne zweiter Stressblock
+    # times[17888542]-times[14270812]
+
