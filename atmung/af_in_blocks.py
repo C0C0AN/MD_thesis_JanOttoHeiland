38aa08f2e34f76436a1b192cca16ce05dc54fb55
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-11 19:28:15 +0100

    load blocks from CSV filei

--- atmung/af_in_blocks.py
+++ atmung/af_in_blocks.py
@@ -3,8 +3,10 @@
 Zählen der Peaks in Blöcken von zum Beispiel Minuten (60sec)
 """
 from resp import load_vhdr
+from blocks import load_blocks
 from matplotlib import pyplot as plt
 from mne.preprocessing.peak_finder import peak_finder
+from os import path
 
 
 def number_peaks(peak_times, start, stop):
@@ -35,14 +37,18 @@ def af_blocks(fname, dauer=60):
     return l, t
 
 
-def plot_af_blocks(l, t):
+def plot_af(l, t):
     plt.plot(t, l, label="Atemfreq")
     plt.xlabel('Zeit(s)')
     plt.ylabel('Atemfreq [1/min]')
     plt.title("peaks in resp curve")
 
-    plt.gca().axvspan(1261.3976,1984.9446, facecolor='0.8', alpha = 0.5)
-    plt.gca().axvspan(3077.9946,3801.5416, facecolor='0.8', alpha = 0.5)
+
+def plot_blocks(fname):
+    df = load_blocks()
+    s0, t0, s1, t1, s2, t2 = df.loc[path.basename(fname)]
+    plt.gca().axvspan(s0, t0, facecolor='0.8', alpha = 0.5)
+    plt.gca().axvspan(s2, t2, facecolor='0.8', alpha = 0.5)
     plt.legend()
 
 
@@ -50,5 +56,6 @@ if __name__ == '__main__':
     fname = "../../Daten/BekJan/HOAF_16.vhdr"
     dauer = 120
     l, t = af_blocks(fname, dauer=dauer)
-    plot_af_blocks(l, t)
+    plot_af(l, t)
+    plot_blocks(fname)
     plt.show()
