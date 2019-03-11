bdffb4e248fa8d7d2d92df8a440f1cb2f34fec08
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-11 18:49:30 +0100

    Ausgelagert in Methoden

--- atmung/af_in_blocks.py
+++ atmung/af_in_blocks.py
@@ -11,33 +11,44 @@ def number_peaks(peak_times, start, stop):
     return len(peak_times[(peak_times >= start) & (peak_times < stop)])
 
 
-fname = "../../Daten/BekJan/HOAF_16.vhdr"
-dauer = 120
-data, times, raw = load_vhdr(fname)
-datavector = data.reshape(-1)
-threshold = (max(datavector)-min(datavector)) / 30
+def af_blocks(fname, dauer=60):
+    '''
+    return af /min Zeit(xAchse)
+    '''
+    data, times, raw = load_vhdr(fname)
+    datavector = data.reshape(-1)
+    threshold = (max(datavector)-min(datavector)) / 30
 
-peak_loc, peak_mag = peak_finder(datavector,thresh=threshold, extrema=-1)
-peak_times = times[peak_loc]
+    peak_loc, peak_mag = peak_finder(datavector,thresh=threshold, extrema=-1)
+    peak_times = times[peak_loc]
+
+    endzeit = times[-1]
+    start = 0
+    stop = dauer
+    l = []
+    t = []
+    while stop <= endzeit:
+        t.append(0.5*(start + stop))
+        l.append(number_peaks(peak_times, start, stop))
+        start = start + dauer
+        stop = stop + dauer
+    return l, t
 
 
-endzeit = times[-1]
-start = 0
-stop = dauer
-l = []
-t = []
-while stop <= endzeit:
-    t.append(0.5*(start + stop))
-    l.append(number_peaks(peak_times, start, stop))
-    start = start + dauer
-    stop = stop + dauer
+def plot_af_blocks(l, t):
+    plt.plot(t, l, label="Atemfreq")
+    plt.xlabel('Zeit(s)')
+    plt.ylabel('Atemfreq [1/min]')
+    plt.title("peaks in resp curve")
 
-plt.plot(t, l)
-plt.xlabel('Zeit(s)')
-plt.ylabel('Atemfreq [1/min]')
-plt.title("peaks in resp curve")
-plt.legend()
-plt.gca().axvspan(1261.3976,1984.9446, facecolor='0.8', alpha = 0.5)
-plt.gca().axvspan(3077.9946,3801.5416, facecolor='0.8', alpha = 0.5)
+    plt.gca().axvspan(1261.3976,1984.9446, facecolor='0.8', alpha = 0.5)
+    plt.gca().axvspan(3077.9946,3801.5416, facecolor='0.8', alpha = 0.5)
+    plt.legend()
 
-plt.show()
+
+if __name__ == '__main__':
+    fname = "../../Daten/BekJan/HOAF_16.vhdr"
+    dauer = 120
+    l, t = af_blocks(fname, dauer=dauer)
+    plot_af_blocks(l, t)
+    plt.show()
