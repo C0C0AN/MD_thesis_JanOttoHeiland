809160a579fa42bc207afd304d4c1eb0e399df04
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-11 18:33:08 +0100

    works, for the first Proband

--- atmung/af_in_blocks.py
+++ atmung/af_in_blocks.py
@@ -1,7 +1,8 @@
+# coding: utf-8
 """
 Zählen der Peaks in Blöcken von zum Beispiel Minuten (60sec)
 """
-from mne_atmung import number_peak
+from resp import load_vhdr
 from matplotlib import pyplot as plt
 from mne.preprocessing.peak_finder import peak_finder
 
@@ -14,17 +15,8 @@ peak_loc, peak_mag = peak_finder(datavector,thresh=threshold, extrema=-1)
 peak_times = times[peak_loc]
 
 
-
-#def _peak_detection():
-#    start = 0
-#    stop = 10000
-#
 def number_peaks(start, stop):
     return len(peak_times[(peak_times >= start) & (peak_times < stop)])
-#
-#    pt_1 = peak_times[(peak_times>=start )& (peak_times<=stop)]
-#
-#    len(pt_1)
 
 endzeit = times[-1]
 
@@ -38,16 +30,12 @@ while stop <= endzeit:
     start = start + 60
     stop = stop + 60
 
-
-plt.plot(x,label="stress_1")
+plt.plot(t, l)
 plt.xlabel('Zeit(s)')
-plt.ylabel('Atmung')
-
+plt.ylabel('Atemfreq [1/min]')
 plt.title("peaks in resp curve")
-
 plt.legend()
-plt.legend()
-plt.gca().axvspan(1261.3976,1984.9446 facecolor='0.8',alpha = 0.5)
-plt.gca().axvspan(3077.9946,3801.5416, facecolor='0.8',alpha = 0.5)
+plt.gca().axvspan(1261.3976,1984.9446, facecolor='0.8', alpha = 0.5)
+plt.gca().axvspan(3077.9946,3801.5416, facecolor='0.8', alpha = 0.5)
 
 plt.show()
