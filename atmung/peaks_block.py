bbf1dabd68e94ace40466e0e3ca7642119fa940e
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-12 19:28:32 +0100

    'Änderung vorgenommen, dass der Moving average in den Zeitleisten mit berücksichtigt ist, das alle Curven in einem Plot sind'

--- atmung/peaks_block.py
+++ atmung/peaks_block.py
@@ -5,7 +5,6 @@ Created on Tue Mar 12 10:45:01 2019
 
 @author: jan
 """
-
 from blocks import load_blocks
 import mne 
 from af_in_blocks import af_blocks
@@ -24,13 +23,12 @@ plt.xlabel('Zeit(s)')
 plt.ylabel('Af [1/120s')
 plt.title("Af Stressblock 1")
 
-def peaks_in_block(fname):
+def peaks_in_block(fname, dauer=60):
     df = load_blocks()
     s0, t0, s1, t1, s2, t2 = df.loc[path.basename(fname)]
-    freq, point = af_blocks(fname, start=s0, endzeit=t0)
-    fname = "../../Daten/BekJan/"
-    endzeit = t0
-    return freq, point
+    freq, point = af_blocks(fname, dauer=dauer, start=s2, endzeit=t2)
+    point = np.array(point)
+    return freq, point - point[0] + 0.5 * dauer
 
 
 for filepath in glob.iglob('../../Daten/BekJan/*.vhdr'):
@@ -39,33 +37,28 @@ for filepath in glob.iglob('../../Daten/BekJan/*.vhdr'):
         print(i)
 
 plt.plot(point, 20+np.array(freq))
-#
 
-#
-#fig, ax = plt.subplots()
-#ax.plot(y, label='stress_2')
-#ax.plot(x, label='stress_1')
-#
-#plt.xlabel('Zeit(s)')
-#plt.ylabel('Puls(bpm)')
-#
-#plt.title("Pulsdaten Stressblock 1+2")
-#
-#plt.legend()
-#plt.gca().axvspan(0,177.72852, facecolor='0.8',alpha = 0.5)
-#plt.gca().axvspan(353.2768,529.1018, facecolor='0.8',alpha = 0.5)
 
 if __name__ == '__main__':
     process_all = True
     if process_all:
-        list_blocks = []
-        for filepath in glob.iglob('../../Daten/BekJan/*.vhdr'):
+        ls= []     
+        filenames = [path.join('../../Daten/BekJan/', f) 
+                     for f in load_blocks().index]
+        for filepath in filenames:
             ls.append(peaks_in_block(filepath))
             plt.plot(point,freq)
-            print(ls)
+            plt.xlabel('Zeit(s)')
+            plt.ylabel('Af 1/120s')
+            plt.title("Af Stressblock 2 alle P")
+#            plt.gca().axvspan(0,177.72852, facecolor='0.8',alpha = 0.5)
+#            plt.gca().axvspan(353.2768,529.1018, facecolor='0.8',alpha = 0.5)
+#            plt.gca().axvspan(0,177.72852, facecolor='0.8',alpha = 0.5)
+#            plt.gca().axvspan(353.2768,529.1018, facecolor='0.8',alpha = 0.5)
+            #print(ls)
         plt.show()
         
     else:
-        load_blocks("../../Daten/BekJan/HOAF_13.vhdr")
+        load_blocks("../../Daten/BekJan/HOAF_19.vhdr")
     
 
