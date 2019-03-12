46ee699137914c7f64e25225c6abf5b285aa11ba
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-12 16:41:16 +0100

    'Liste für alle Probanden möglich'

--- atmung/peaks_block.py
+++ atmung/peaks_block.py
@@ -16,40 +16,61 @@ def peaks_in_block(fname):
     df = load_blocks()
     s0, t0, s1, t1, s2, t2 = df.loc[path.basename(fname)]
     freq, point = af_blocks(fname, start=s0, endzeit=t0)
-    fname = "../../Daten/BekJan/HOAF_16.vhdr"
+    fname = "../../Daten/BekJan/"
     endzeit = t0
     return freq, point
 
 %matplotlib
 plt.plot(point, freq)
 
-plt.plot(point, 20+np.array(freq))
+for i in peaks_in_block("../../Daten/BekJan/"):
+    print(peaks_in_block)
+    
+    list_blocks.append(peaks_in_block(fname))
+    print(peaks_in_block)
+    
+for i in os.listdir('../../Daten/BekJan/'):
+for filename in os.listdir('../../Daten/BekJan/'):
+    if filename.endswith(".vhdr")
+        #print(os.path.join(directory, filename))
+        continue
+    else:
+        continue
 
+import glob
 
-x  =extractedData
-y = extractedData_2
-z = extractedData_3
+for filepath in glob.iglob('../../Daten/BekJan/*.vhdr'):
+    print(filepath)
+    for i in peaks_in_block('../../Daten/BekJan/'):
+        print(i)
 
-fig, ax = plt.subplots()
-ax.plot(y, label='stress_2')
-ax.plot(x, label='stress_1')
-
-plt.xlabel('Zeit(s)')
-plt.ylabel('Puls(bpm)')
-
-plt.title("Pulsdaten Stressblock 1+2")
-
-plt.legend()
-plt.gca().axvspan(0,177.72852, facecolor='0.8',alpha = 0.5)
-plt.gca().axvspan(353.2768,529.1018, facecolor='0.8',alpha = 0.5)
-
+#plt.plot(point, 20+np.array(freq))
+#
+#
+#x  =extractedData
+#y = extractedData_2
+#z = extractedData_3
+#
+#fig, ax = plt.subplots()
+#ax.plot(y, label='stress_2')
+#ax.plot(x, label='stress_1')
+#
+#plt.xlabel('Zeit(s)')
+#plt.ylabel('Puls(bpm)')
+#
+#plt.title("Pulsdaten Stressblock 1+2")
+#
+#plt.legend()
+#plt.gca().axvspan(0,177.72852, facecolor='0.8',alpha = 0.5)
+#plt.gca().axvspan(353.2768,529.1018, facecolor='0.8',alpha = 0.5)
 
 if __name__ == '__main__':
     process_all = True
     if process_all:
         list_blocks = []
-        for fname in peaks_in_block('../../Daten/BekJan/'):
-            list_blocks.append(peaks_in_block(fname))
+        for filepath in glob.iglob('../../Daten/BekJan/*.vhdr'):
+        ls.append(peaks_in_block(filepath))
+        print(ls)
     else:
         load_blocks("../../Daten/BekJan/HOAF_13.vhdr")
     
