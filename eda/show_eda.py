8fbeedaaf6a4a4179bf006c59347476fcf20f69a
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-14 14:53:56 +0100

    plot eda data for HOAF_16

--- eda/show_eda.py
+++ eda/show_eda.py
@@ -6,16 +6,8 @@ Created on Thu Mar 14 13:37:06 2019
 @author: jan
 """
 
-import mne
-import os.path as op
 import numpy as np
 from matplotlib import pyplot as plt
-import shutil
-#%matplotlib
-from mne.io import read_raw_brainvision
-from resp import fix_ch_bug
-from numpy.testing import assert_array_equal
-import os
 import numpy as np
 from resp import raw_vhdr
 
@@ -30,19 +22,10 @@ def load_eda(fname, verbose=True):
     return data[0], times, raw
 
 
-file = "../../Daten/BekJan/HOAF_16.vhdr"
-raw = read_raw_brainvision(file, preload=True)
-
-# Laden der roh Daten, hier noch in mehreren Dimensionen
-data = raw.get_data()
-
-#Definiere dass nur der respiratorische Channel gnutzt wird
-raw.pick_channels(['GSR_MR_100'])
-
-fig = raw.plot(None, 10.0,0.0,3,'w',None,(0.8,0.8,0.8),'cyan','auto')
-
-ch_labels = ['GSR_MR_100', 'Resp', 'Stim']
-ch_types = ['misc', 'misc', 'stim']
-sfreq = 250
-
+if __name__ == '__main__':
+    fname = "../../Daten/BekJan/HOAF_16.vhdr"
+    data, times, _ = load_eda(fname)
+    plt.plot(times, data)
+    plt.show()
+    
 
