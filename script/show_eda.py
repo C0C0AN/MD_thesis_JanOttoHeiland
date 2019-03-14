123113aa8b8ec969d31be3b04e4e779404375109
Jan Otto Heiland <otto.heiland@gmx.de>
2019-03-14 16:51:29 +0100

    reduce_mean

--- script/show_eda.py
+++ script/show_eda.py
@@ -5,11 +5,17 @@ Created on Thu Mar 14 13:37:06 2019
 
 @author: jan
 """
-
-import numpy as np
 from matplotlib import pyplot as plt
-import numpy as np
 from resp import raw_vhdr
+from os import path
+
+
+def reduce_mean(a, size=1000):
+    rest = len(a) % size
+    if rest != 0:
+        a = a[:-rest]   # elemente abschneiden, sodass len(a) Vielfaches von size
+    a = a.reshape(-1, size)
+    return a.mean(axis=1)
 
 
 def load_eda(fname, verbose=True):
@@ -25,7 +31,7 @@ def load_eda(fname, verbose=True):
 if __name__ == '__main__':
     fname = "../../Daten/BekJan/HOAF_16.vhdr"
     data, times, _ = load_eda(fname)
-    plt.plot(times, data)
+    s=5000;
+    plt.title(path.basename(fname) + ", s=" + str(s))
+    plt.plot(reduce_mean(times, size=s), reduce_mean(data, size=s))
     plt.show()
-    
-
