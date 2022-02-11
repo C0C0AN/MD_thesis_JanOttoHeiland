74ee3d4977aab4415b62b40982ed7562bb8bbad6
Jan Otto Heiland <otto.heiland@gmx.de>
2022-02-11 17:57:12 +0100

    Standardmäßig Mittelwerte berechnen beim Laden

--- script/phases_analyze.py
+++ script/phases_analyze.py
@@ -2,16 +2,17 @@
 import pandas as pd
 
 
-def load_phasen(file_name="phasen.tsv"):
+def load_phasen(file_name="phasen.tsv", means=True):
     pf = pd.read_csv(file_name, sep="\t")
     pf = pf.set_index(list(pf.columns[:5]))
     pf = pf.sort_values(["prob_nr", "run", "start"])
+    if means:
+        return pf.groupby(level=[1, 2, 3, 4], sort=False).mean()
     return pf
 
 
 if __name__ == "__main__":
-    pf = load_phasen("phasen.tsv")
-    runs = pf.groupby(level=[1, 2, 3, 4], sort=False).mean()
+    runs = load_phasen("phasen.tsv", means=True)
     r1, r2 = runs.loc[1], runs.loc[2]
     print(
         "maximale Abweichung zwischen den Runs",
