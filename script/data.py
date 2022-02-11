0189b1cfa634698df5f9a247813823de4c8ecf4f
Jan Otto Heiland <otto.heiland@gmx.de>
2022-02-11 19:14:23 +0100

    more directories in data.py

--- script/data.py
+++ script/data.py
@@ -3,5 +3,7 @@ import toml
 from os import path
 
 
-config = toml.load(path.join(path.dirname(__file__), "config.toml"))
+config = toml.load(path.join(path.dirname(path.realpath(__file__)), "config.toml"))
 PREFIX = config["PREFIX"]
+HOAF_DIR = path.join(PREFIX, "HOAF")
+HOAF_BIDS = path.join(HOAF, "HOAF_BIDS")
