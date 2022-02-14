6d3a0b5fda66d651e1e037e1248b1b0694a20aeb
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-02-14 14:32:23 +0100

    PREFIX is relativ zum Konfigurationsverzeichnis

--- script/data.py
+++ script/data.py
@@ -6,9 +6,14 @@ from os import path
 config_dir = path.dirname(path.realpath(__file__))
 config_toml = path.join(config_dir, "config.toml")
 if not path.exists(config_toml):
+    # Wenn keine Konfiguration `config.toml` erstellt wurde, nehme die Beispielkonfiguration
     config_toml = path.join(config_dir, "config.example.toml")
 config = toml.load(config_toml)
+
 PREFIX = config["PREFIX"]
+if not path.isabs(PREFIX):
+    # Bei einem relativen Pfad: füge das Konfigurationsverzeichnis vorne dran
+    PREFIX = path.join(config_dir, PREFIX)
 HOAF_DIR = path.join(PREFIX, "HOAF")
 HOAF_BIDS = path.join(HOAF_DIR, "HOAF_BIDS")
 DATEN_DIR = config["DATEN"]
