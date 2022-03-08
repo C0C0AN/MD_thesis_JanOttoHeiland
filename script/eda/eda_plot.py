e9074c8831e04e0f688491e7614d3a50a5da3047
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-03-08 16:07:44 +0100

    plotte nur die ersten fünf Probanden

--- script/eda/eda_plot.py
+++ script/eda/eda_plot.py
@@ -4,11 +4,17 @@ import pandas as pd
 lf = pd.read_csv("eda_long.tsv", sep="\t")
 
 block2 = lf[lf.block == 1].drop(columns=["block"]).copy()
+block2 = block2.reset_index().drop(columns=["index"])
 prob_ids = sorted(block2.prob_id.unique())
 for p in prob_ids:
     rows = block2.prob_id == p
     block2.loc[rows, "time"] -= block2.loc[rows, "time"].min()
 
-df = pd.pivot(block2, index="prob_id", columns=["time"])
-df.T.plot()
+df = pd.pivot(block2, index="prob_id", columns=["time"], values="eda")
+df_complete = df
+
+(df.T - df.mean(axis=1)).plot()
+df = df.loc[df.index[:5]]
+plt.figure()
+((df.T - df.mean(axis=1)) / df.std(axis=1)).plot()
 plt.show()
