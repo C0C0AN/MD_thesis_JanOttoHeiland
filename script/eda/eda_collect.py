a37bc2c66a0d3e229fb6369833b15d70a7f63cae
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-03-08 15:17:50 +0100

    Schreibe die EDA Blöcke raus im Long-Format

--- script/eda/eda_collect.py
+++ script/eda/eda_collect.py
@@ -1,43 +1,69 @@
 """Sammel EDA Daten und schreibe es in CSV Datei."""
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from blocks import load_blocks
-from show_eda import DATEN, load_eda, compute_reduced_eda
+from show_eda import DATEN, compute_reduced_eda, load_eda
 
-blocks = load_blocks()
-keep = blocks.index.str.match(r"HOAF_\d.+vhdr")
-blocks.drop(blocks[~keep].index, inplace=True)
-blocks[0] = 0.0
-blocks = blocks.reindex(columns=range(6))
+
+def index_prob_id(blocks):
+    """Set prob_id in index."""
+    return blocks.set_index(
+        pd.Index(
+            blocks.index.str.extract(r"HOAF_(\d\d)\.vhdr").astype(int)[0],
+            name="prob_id",
+        )
+    )
 
 
 def _vhdr_filename(prob_id):
-    return DATEN + "/BekJan/" + prob_id
+    return f"{DATEN}/BekJan/HOAF_{prob_id}.vhdr"
 
 
-def load_reduced_eda() -> list[tuple[np.ndarray, np.ndarray]]:
+def load_reduced_eda(prob_ids) -> list[tuple[np.ndarray, np.ndarray]]:
     """Reduzierte Datenpunkte.
 
     Für jeden Probanden, `times, data`.
     """
-    filenames = [_vhdr_filename(p) for p in blocks.index]
+    filenames = [_vhdr_filename(p) for p in prob_ids]
     points_per_sec = 5000
     return compute_reduced_eda(points_per_sec, filenames)
 
 
-def _wip():
-    eda = load_reduced_eda()
-    df = pd.DataFrame(data=[e[1] for e in eda], index=blocks.index)
-    for i, (prob_id, df) in enumerate(blocks.iterrows()):
-        assert (np.gradient(times) > 0).all()  # Prüfe, dass times sortiert
-        intervals = pd.IntervalIndex.from_breaks(blocks.loc[prob_id])
-        a, b = np.searchsorted(times, [intervals[2].left, intervals[2].right])
+def eda_long(blocks) -> pd.DataFrame:
+    """Load all EDA data in long format."""
+    eda = load_reduced_eda(blocks.index)
+    ef = pd.DataFrame(data=[e[1] for e in eda], index=blocks.index)
+    lf = (
+        ef.reset_index()
+        .melt(id_vars=["prob_id"], value_name="eda", var_name="time")
+        .dropna()
+    )
+    lf["block"] = pd.NA
+    for i, (pid, breaks) in enumerate(blocks.iterrows()):
+        breaks[0] = -0.001
+        intervals = pd.IntervalIndex.from_breaks(breaks)
+        rows = lf.prob_id == pid
+        block_codes = pd.cut(lf.loc[rows, "time"], intervals).cat.codes
+        lf.loc[rows, "block"] = block_codes
+    lf.block = lf.block.astype("int8")
+    return lf
 
 
 if __name__ == "__main__":
-    duration = pd.DataFrame(index=blocks.index)
-    blocks[0] = 0
+    bs = load_blocks()
+    keep = bs.index.str.match(r"HOAF_\d.+vhdr")
+    bs.drop(bs[~keep].index, inplace=True)
+    bs[0] = 0.0
+    bs = bs.reindex(columns=sorted(bs.columns))
+    bs.index.set_names("file", inplace=True)
+
+    duration = pd.DataFrame(index=bs.index)
     for i in range(6):
-        duration[i] = blocks[i + 1] - blocks[i]
+        duration[i] = bs[i + 1] - bs[i]
     duration.to_csv("duration.tsv", sep="\t", float_format="%.2f")
+
+    blocks = index_prob_id(bs)
+    lf = eda_long(blocks)
+    lf.to_csv("eda_long.tsv", sep="\t", float_format="%9.7f", index=False)
