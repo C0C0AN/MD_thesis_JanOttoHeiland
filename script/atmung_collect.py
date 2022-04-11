5412416b077fc00084433a63861062f3343540f8
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2022-04-11 22:41:47 +0200

    mache eine neue Funktion `concat_all_csvs` draus

--- script/atmung_collect.py
+++ script/atmung_collect.py
@@ -1,23 +1,30 @@
 """Zusammentragen aller Daten zu den Atmungswerten."""
-import pandas as pd
 from os import path
 
+import pandas as pd
+
+from collect import load_group_info, load_runs, run_intervals
 from data import HOAF_BIDS
 
 
-RESP_DIR = f"{HOAF_BIDS}/derivatives/physio/resp"
-dfs = list()
-for i in range(1, 30):
-    for r in [1, 2]:
-        csv = f"{RESP_DIR}/sub-{i:02}/sub-{i:02}_task-stress_run-{r}_physio_resp.csv"
-        if path.exists(csv):
-            d = pd.read_csv(csv, header=None, names=["resp"])
-            d["run"] = r
-            d["prob_id"] = i
-            d.reset_index(inplace=True)
-            d["time"] = d["index"] * 1.45
-            d.drop(columns=["index"], inplace=True)
-            dfs.append(d)
+def concat_all_csvs(RESP_DIR = f"{HOAF_BIDS}/derivatives/physio/resp"):
+    """Kombiniere alle Einzelwerte in ein DataFrame."""
+    dfs = list()
+    for i in range(1, 30):
+        for r in [1, 2]:
+            csv = f"{RESP_DIR}/sub-{i:02}/sub-{i:02}_task-stress_run-{r}_physio_resp.csv"
+            if path.exists(csv):
+                d = pd.read_csv(csv, header=None, names=["resp"])
+                d["run"] = r
+                d["prob_id"] = i
+                d.reset_index(inplace=True)
+                d["time"] = d["index"] * 1.45
+                d.drop(columns=["index"], inplace=True)
+                dfs.append(d)
 
-assert {len(df) for df in dfs} == {500}
-r = pd.concat(dfs)
+    assert {len(df) for df in dfs} == {500}
+    return pd.concat(dfs)
+
+
+if __name__ == "__main__":
+    df = concat_all_csvs()
