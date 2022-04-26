156bf43fabb68d2f183e1bbb4069c2ec61abe403
Jan Otto Heiland <otto.heiland@gmx.de>
2022-04-26 12:54:19 +0200

    Runs separate in Pulsdaten

--- script/puls/puls_baseline.py
+++ script/puls/puls_baseline.py
@@ -8,10 +8,14 @@ if __name__ == "__main__":
     fname = "puls_long.tsv"
     df = pd.read_csv(fname, sep="\t")
     probanden = df["prob_nr"].unique()
+    run = df["run"].unique()
+    
     for p in probanden:
-        # wähle alle Zeilen mit entsprechender `prob_nr` in der Spalte `puls`
-        rows, cols = df["prob_nr"] == p, "puls"
-        mean = df.loc[rows, cols].mean()
-        print(f"Proband {p:02}: Mittelwert {mean:6.2f}")
-        df.loc[rows, cols] = df.loc[rows, cols] - mean
-    df.to_csv("puls_long_basecorrect.tsv", sep="\t", float_format="%0.2f", index=False)
+        for r in run:
+            rows = (df["prob_nr"] == p) & (df["run"] == r)
+            cols = "puls"
+            mean = df.loc[rows, cols].mean()
+            print(f"Proband {p:02}: run{r:1}: Mittelwert {mean:6.2f}")
+            df.loc[rows, cols] = df.loc[rows, cols] - mean
+    df.to_csv("puls_long_runcorrect.tsv", sep="\t", float_format="%0.2f", index=False)
+
